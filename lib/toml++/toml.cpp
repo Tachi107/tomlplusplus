ce321c323c518eebd38c11d667f34e7e2d76cc2e
Andrea Pappacoda <andrea@pappacoda.it>
2022-02-01 22:39:54 +0100

    wip: implementing compile_library
    https://github.com/marzer/tomlplusplus/issues/140#issuecomment-1025954587

---
+++ lib/toml++/toml.cpp
@@ -0,0 +1,9 @@
+#if !defined TOML_HEADER_ONLY || TOML_HEADER_ONLY == 1
+#	undef TOML_HEADER_ONLY
+#	define TOML_HEADER_ONLY 0
+#endif
+#ifndef TOML_IMPLEMENTATION
+#define TOML_IMPLEMENTATION
+#endif
+
+#include <toml++/toml.h>
